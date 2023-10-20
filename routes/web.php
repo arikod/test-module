2c5c1932845727c88577cd21d779d92d85b4cc5d
Bekir Gülmüş <bekir.gulmus@hotmail.com>
2023-10-20 17:40:04 +0300

    Update web.php
    update

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('testModule',function (){
-<<<<<<< HEAD
-    return 'Hello Worldd2';
-=======
-    return 'Hello Worldd';
->>>>>>> main
+    return 'Hello World';
 });
